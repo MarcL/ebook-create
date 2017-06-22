4dd99279f80472c0aeac09f40ea218d8cd2f06ec
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-06-22 22:21:01 +0100

    Move markdown-pdf options into function

--- src/leadMagnet.js
+++ src/leadMagnet.js
@@ -2,11 +2,6 @@ import markdownPdf from 'markdown-pdf';
 import through from 'through';
 import path from 'path';
 
-var markDownToPdfoptions = {
-    preProcessMd: preProcessMd,
-    runningsPath: path.join(__dirname, './pageRunnings.js')
-};
-
 function addPageBreaks(data) {
     const pageBreak = '\n\n<div style="page-break-before: always;"></div>\n\n';
     this.queue(`${data}${pageBreak}`);
@@ -17,6 +12,11 @@ function preProcessMd() {
 }
 
 function createPdf(files, outputFile) {
+    var markDownToPdfoptions = {
+        preProcessMd: preProcessMd,
+        runningsPath: path.join(__dirname, './pageRunnings.js')
+    };
+
     markdownPdf(markDownToPdfoptions).concat.from(files).to(outputFile, () => {
         console.log(`Created : ${outputFile}`);
     });

61bb353dafc78493d527f9cba22e8129d3db0d48
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-06-23 20:18:43 +0100

    Move PDF setup to constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const constants = {
+    pdf: {
+        format: 'A4',        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+        orientation: 'portrait', // portrait or landscape
+        border: {
+            top: '1.9cm',
+            left: '1.9cm',
+            right: '1.32cm',
+            bottom: '3.67cm'
+        }
+    }
+};
+
+export default constants;

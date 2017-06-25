7151bb9d410ac4fc36bbabf9941ef6f6339648c6
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-06-25 17:14:52 +0100

    Refactor PDF conversion

--- src/ebook.js
+++ src/ebook.js
@@ -1,62 +1,69 @@
-import pdf from 'html-pdf';
+import htmlToPdf from 'html-pdf';
+
+function convertHtmlToPDF(contents, options, filePath) {
+    return new Promise((resolve, reject) => {
+        console.log(`-- Converting : ${filePath}`);
+        htmlToPdf
+            .create(contents, options)
+            .toFile(filePath, (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(contents, options);
+                }
+            });
+    });
+};
 
 function ebookPlugin(options) {
     return function(files, metalsmith, done) {
         const destinationPath = `${metalsmith.destination()}/`;
         const metadata = metalsmith.metadata();
 
-        // THROW ERROR IF TITLE ATTRIBUTE IS NOT SET
         if (!metadata.title) {
             return done(new Error('Please set metadata title'));
         }
 
+        // TODO:
+        // - Convert files object to array
+        // - Filter files to remove non-HTML names
+        // - Create promise for each file conversion
+        // - Promise.all() on all promise to convert
+        // - Catch any errors
+
         // console.log(files);
+        let filePromises = [];
         Object.keys(files).forEach(file => {
             if (file.includes('.html')) {
                 var contents = files[file].contents.toString();
                 var path = files[file].path + '/';
                 var title = files[file].title;
 
-                var p0 = new Promise((resolve, reject) => {
-                    let filePath;
+                let filePath;
 
-                    if (files[file].path) {
-                        filePath = destinationPath + path + title + '.pdf';
-                    } else {
-                        const bookTitle = metadata.title;
-                        filePath = destinationPath + bookTitle + '.pdf';
-                    }
+                if (files[file].path) {
+                    filePath = destinationPath + path + title + '.pdf';
+                } else {
+                    const bookTitle = metadata.title;
+                    filePath = destinationPath + bookTitle + '.pdf';
+                }
 
-                    const pdfOptions = Object.assign({}, options.pdf, {
-                        base: `file://${destinationPath}`
-                    });
-
-                    createPDF(contents, pdfOptions, filePath);
-
-                    function createPDF(contents, options, filePath) {
-                        console.log(`-- Writing : ${filePath}`);
-                        pdf
-                            .create(contents, options)
-                            .toFile(filePath, (error, res) => {
-                                if (error) {
-                                    reject();
-                                } else {
-                                    resolve(contents, options);
-                                }
-                            });
-                    }
+                const pdfOptions = Object.assign({}, options.pdf, {
+                    base: `file://${destinationPath}`
                 });
 
-                p0
-                    .then(function() {
-                        console.log('PDF CREATED');
-                    })
-                    .catch(error => {
-                        console.log('ERROR GENERATING PDF');
-                        console.log(`\t${error}`);
-                    });
+                filePromises.push(convertHtmlToPDF(contents, pdfOptions, filePath));
             }
         });
+
+        Promise.all(filePromises)
+            .then(() => {
+                console.log('Converted all files');
+            })
+            .catch((error) => {
+                console.log('Error generating PDF');
+                console.log(`\t${error}`);
+            });
 
         done();
     };

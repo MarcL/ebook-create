9db55fcafa999b5ff75acfac2ee64a9e2a006228
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-06-27 23:13:55 +0100

    Add chalk for CLI colours

--- src/convertHtmlToPdf.js
+++ src/convertHtmlToPdf.js
@@ -1,13 +1,14 @@
+import chalk from 'chalk';
 import htmlToPdf from 'html-pdf';
 import logSymbols from 'log-symbols';
 
 function convertHtmlToPDF(contents, options, filePath) {
     return new Promise((resolve, reject) => {
-        console.log(`${logSymbols.info} Converting : ${filePath}`);
+        console.log(`${logSymbols.info} ${chalk.bold(`Converting : ${filePath}`)}`);
         htmlToPdf.create(contents, options).toFile(filePath, error => {
             const message = error
-                ? `${logSymbols.error} Error converting : ${filePath} : ${error.message}`
-                : `${logSymbols.success} Converted : ${filePath}`;
+                ? `${logSymbols.error} ${chalk.red(`Error converting : ${filePath} : ${error.message}`)}`
+                : `${logSymbols.success} ${chalk.green(`Converted : ${filePath}`)}`;
 
             resolve(message);
         });
